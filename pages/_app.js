27e46f207cc0ac43b321e85a8c841e1589c02ba1
halilgoksu <halilskywater@gmail.com>
2022-12-19 16:12:59 -0700

    not sure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactTostify.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
   return (
