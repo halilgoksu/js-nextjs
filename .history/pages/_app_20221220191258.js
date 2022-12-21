80db6b6b4515cccc4cad696a34a6c4c108f7c8b8
halilgoksu <halilskywater@gmail.com>
2022-12-20 19:18:31 -0700

    not sure

---
+++ .history/pages/_app_20221220191258.js
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+import Layout from "../components/layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+  );
+}
+
+export default MyApp;
---
+++ .history/pages/_app_20221220191305.js
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import Layout from "../components/layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function MyApp({ Component, pageProps }) {
+  return (<div>
+    <h2>sadfasdf</h2>
+  </div>
+  );
+}
+
+export default MyApp;
